8882754c937d467cdb81f5ab0ee507fd4b94a675
Marco <mgarcia22@ucol.mx>
2023-11-13 13:23:20 -0600

    Hints y botones login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -34,7 +34,7 @@ class _LoginFormState extends State<LoginForm> {
     // For a basic example, let's just print the entered credentials.
     print('Username: $username, Password: $password');
   }
-
+  
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -45,7 +45,7 @@ class _LoginFormState extends State<LoginForm> {
           "Iniciar Sesión",
           style: Theme.of(context).textTheme.titleLarge,
         ),
-
+        const SizedBox(height: 30),
         // Usuario
         Row(
           children: [
@@ -55,7 +55,11 @@ class _LoginFormState extends State<LoginForm> {
               child: TextField(
                 controller: usernameController,
                 decoration:
-                    const InputDecoration(contentPadding: EdgeInsets.all(10)),
+                  const InputDecoration(
+                    hintText: 'Username', 
+                    hintStyle: TextStyle(color: Color.fromARGB(255, 178, 178, 178)),
+                    contentPadding: EdgeInsets.all(10)
+                  ),
               ),
             ),
             const Expanded(flex: 1, child: SizedBox()),
@@ -63,7 +67,7 @@ class _LoginFormState extends State<LoginForm> {
         ),
 
         // Espacio
-        const SizedBox(height: 50),
+        const SizedBox(height: 30),
 
         // Contraseña
         Row(
@@ -75,7 +79,11 @@ class _LoginFormState extends State<LoginForm> {
                 obscureText: true,
                 controller: passwordController,
                 decoration:
-                    const InputDecoration(contentPadding: EdgeInsets.all(10)),
+                  const InputDecoration(
+                    hintText: 'Contraseña', 
+                    hintStyle: TextStyle(color: Color.fromARGB(255, 178, 178, 178)),
+                    contentPadding: EdgeInsets.all(10)
+                  ),
               ),
             ),
             const Expanded(flex: 1, child: SizedBox()),
@@ -83,7 +91,7 @@ class _LoginFormState extends State<LoginForm> {
         ),
 
         // Espacio
-        const SizedBox(height: 50),
+        const SizedBox(height: 30),
 
         // Boton iniciar login
         Row(
@@ -98,6 +106,22 @@ class _LoginFormState extends State<LoginForm> {
             const Expanded(flex: 1, child: SizedBox()),
           ],
         ),
+        // Boton de contraseña olvidada y registro
+        const SizedBox(height: 10),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () { },
+                child: Text('¿Olvidaste tu contraseña?'),
+              ),
+              Text('|'),
+              TextButton(
+                onPressed: () { },
+                child: Text('Registrarse'),
+              ),
+            ],
+          ),
       ],
     );
   }
